3bab2f0023250766173609d611fbbbefcfc558b7
JosephEspinoza99 <josephstevenespinozaoviedo@gmail.com>
2023-09-18 18:59:51 -0500

    a

--- app/src/main/java/com/example/tensorflow2/MainActivity.java
+++ app/src/main/java/com/example/tensorflow2/MainActivity.java
@@ -16,9 +16,11 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.example.tensorflow2.ml.Exam;
 import com.example.tensorflow2.ml.Flowers;
 
 import com.example.tensorflow2.ml.Heroes;
+import com.example.tensorflow2.ml.Lab;
 import com.example.tensorflow2.ml.Prueba;
 import com.example.tensorflow2.ml.Tomatodo;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -34,11 +36,17 @@ import com.google.mlkit.vision.text.TextRecognizer;
 import com.google.mlkit.vision.text.latin.TextRecognizerOptions;
 
 import org.tensorflow.lite.DataType;
+import org.tensorflow.lite.support.image.ImageProcessor;
 import org.tensorflow.lite.support.image.TensorImage;
+import org.tensorflow.lite.support.image.ops.ResizeOp;
+import org.tensorflow.lite.support.image.ops.Rot90Op;
 import org.tensorflow.lite.support.label.Category;
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer;
 
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.text.DecimalFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -129,103 +137,93 @@ public class MainActivity
         }
         txtResults.setText(resultados);
     }
-    public void Rostrosfx(View v) {
-        BitmapDrawable drawable = (BitmapDrawable) mImageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap().copy(Bitmap.Config.ARGB_8888,true);
-        Canvas canvas = new Canvas(bitmap);
-        Paint paint = new Paint();
-        paint.setColor(Color.BLUE);
-        paint.setStrokeWidth(15);
-        paint.setStyle(Paint.Style.STROKE);
 
-        InputImage image = InputImage.fromBitmap(mSelectedImage, 0);
-        FaceDetectorOptions options =
-                new FaceDetectorOptions.Builder()
-                        .setPerformanceMode(FaceDetectorOptions.PERFORMANCE_MODE_ACCURATE)
-                        .setContourMode(FaceDetectorOptions.CONTOUR_MODE_ALL)
-                        .build();
-        FaceDetector detector = FaceDetection.getClient(options);
-        detector.process(image)
-                .addOnSuccessListener(new OnSuccessListener<List<Face>>() {
-                    @Override
-                    public void onSuccess(List<Face> faces) {
-                        if (faces.size() == 0) {
-                            txtResults.setText("No Hay rostros");
-                        }else{
-                            txtResults.setText("Hay " + faces.size() + " rostro(s)");
+    public ByteBuffer convertirImagenATensorBuffer(Bitmap mSelectedImage){
 
-                            for (Face rostro: faces) {
-                                canvas.drawRect(rostro.getBoundingBox(), paint);
-                            }
+        Bitmap imagen = Bitmap.createScaledBitmap(mSelectedImage, 224, 224, true);
+        ByteBuffer byteBuffer = ByteBuffer.allocateDirect(4 * 224 * 224 * 3);
+        byteBuffer.order(ByteOrder.nativeOrder());
 
-                        }
-                    }
-                })
-                .addOnFailureListener(this);
+        int[] intValues = new int[224 * 224];
+        imagen.getPixels(intValues, 0, imagen.getWidth(), 0, 0, imagen.getWidth(), imagen.getHeight());
 
-        mImageView.setImageBitmap(bitmap);
+        int pixel = 0;
+
+        for(int i = 0; i <  imagen.getHeight(); i ++){
+            for(int j = 0; j < imagen.getWidth(); j++){
+                int val = intValues[pixel++]; // RGB
+                byteBuffer.putFloat(((val >> 16) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat(((val >> 8) & 0xFF) * (1.f / 255.f));
+                byteBuffer.putFloat((val & 0xFF) * (1.f / 255.f));
+            }
+        }
+        return  byteBuffer;
+    }
+    public String obtenerEtiquetayProbabilidad(String[] etiquetas, float[] probabilidades){
+
+        float valorMayor=Float.MIN_VALUE;
+        int pos=-1;
+        for (int i = 0; i < probabilidades.length; i++) {
+            if (probabilidades[i] > valorMayor) {
+                valorMayor = probabilidades[i];
+                pos = i;
+            }
+        }
+
+        return "Predicción: " + etiquetas[pos] + ", Probabilidad: " + (new DecimalFormat("0.00").format(probabilidades[pos] * 100)) + "%";
+
     }
 
-    public void PersonalizedModel(View v) {
+    public void PersonalizedModel(View v){
         try {
-            // Cargar el modelo de héroes
-            Heroes model = Heroes.newInstance(getApplicationContext());
 
-            // Aplicar transformaciones a la imagen antes de escalarla
-            Bitmap imagen_preprocesada = preprocessImage(mSelectedImage);
+            //Definir Estiquetas de acuerdo a su archivo "labels.txt" generado por la Plataforma de creación del Modelo
+            String[] etiquetas = {"LABORATORIO INDUSTRIAL","POLIDEPORTIVO","CENTRO MEDICO"};
 
-            // Escalar la imagen preprocesada a 224x224 píxeles
-            Bitmap imagen_escalada = Bitmap.createScaledBitmap(imagen_preprocesada, 224, 224, true);
-
-            // Convertir la imagen escalada en un objeto TensorImage
-            TensorImage imagen = new TensorImage(DataType.FLOAT32);
-            imagen.load(imagen_escalada);
-
-            // Crear un TensorBuffer para la entrada del modelo
+            Exam model = Exam.newInstance(getApplicationContext());
             TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
-            inputFeature0.loadBuffer(imagen.getBuffer());
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
 
-            // Procesar la imagen con el modelo
-            Heroes.Outputs outputs = model.process(inputFeature0);
-
-            // Obtener la salida del modelo como un TensorBuffer
+            Exam.Outputs outputs = model.process(inputFeature0);
             TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
 
-            // Etiquetas de los héroes
-            String[] etiquetas = { "Iron Man", "Thor", "Hulk", "BlackWidow", "Hawkeye" };
+            txtResults.setText(obtenerEtiquetayProbabilidad(etiquetas, outputFeature0.getFloatArray()));
 
-            // Obtener las probabilidades de clasificación
-            float[] probabilidades = outputFeature0.getFloatArray();
-
-            // Crear una lista de etiquetas y probabilidades
-            List<String> resultados = new ArrayList<>();
-            for (int i = 0; i < etiquetas.length; i++) {
-                resultados.add(etiquetas[i] + ": " + probabilidades[i] * 100);
-            }
-
-            // Mostrar los resultados en tu interfaz de usuario (por ejemplo, en un TextView o RecyclerView)
-            String resultadoFinal = TextUtils.join("\n", resultados);
-            txtResults.setText("Resultados:\n" + resultadoFinal);
-
-            // Cerrar el modelo después de usarlo
             model.close();
-        } catch (IOException e) {
-            // Manejar errores en caso de problemas al cargar o procesar el modelo
-            txtResults.setText("Error al procesar Modelo");
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
         }
     }
+    public String obtenerEtiquetayProbabilidad2(String[] etiquetas, float[] probabilidades) {
+        StringBuilder resultado = new StringBuilder();
 
-    private Bitmap preprocessImage(Bitmap image) {
-        // Aplicar transformaciones a la imagen antes de escalarla
-        // Por ejemplo, puedes aplicar aumento de contraste, normalización y recorte aquí.
-        // Devuelve la imagen preprocesada.
-        // Aquí tienes un ejemplo simple de escala:
-        int targetSize = 224;
-        Bitmap scaledImage = Bitmap.createScaledBitmap(image, targetSize, targetSize, true);
-        return scaledImage;
+        for (int i = 0; i < etiquetas.length; i++) {
+            resultado.append(etiquetas[i]).append(": ").append(probabilidades[i] * 100).append("%\n");
+        }
+
+        return resultado.toString();
     }
 
+    public void PersonalizedModel2(View v){
+        try {
 
+            //Definir Estiquetas de acuerdo a su archivo "labels.txt" generado por la Plataforma de creación del Modelo
+            String[] etiquetas = {"LABORATORIO INDUSTRIAL","POLIDEPORTIVO","CENTRO MEDICO"};
+
+            Exam model = Exam.newInstance(getApplicationContext());
+            TensorBuffer inputFeature0 = TensorBuffer.createFixedSize(new int[]{1, 224, 224, 3}, DataType.FLOAT32);
+            inputFeature0.loadBuffer(convertirImagenATensorBuffer(mSelectedImage));
+
+            Exam.Outputs outputs = model.process(inputFeature0);
+            TensorBuffer outputFeature0 = outputs.getOutputFeature0AsTensorBuffer();
+
+            txtResults.setText(obtenerEtiquetayProbabilidad2(etiquetas, outputFeature0.getFloatArray()));
+
+            model.close();
+        } catch (Exception e) {
+            txtResults.setText(e.getMessage());
+        }
+    }
 
     class CategoryComparator implements java.util.Comparator<Category> {
         @Override
